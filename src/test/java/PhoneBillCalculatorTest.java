8c1401990aa0774be8372853f8accfc4385a8af8
JeongInn Lee <aretias.kr@gmail.com>
2013-10-14 01:47:07 +0900

    modify introducing testcase

--- src/test/java/PhoneBillCalculatorTest.java
+++ src/test/java/PhoneBillCalculatorTest.java
@@ -26,22 +26,22 @@ public class PhoneBillCalculatorTest {
 	}
 	
 	@Test
-	public void should_return_0_when_basicRate_0_additionalLinesRate_0_and_excessMinutesRate_is_3() throws Exception {
+	public void should_return_9_when_every_rate_is_3() throws Exception {
 		PhoneBillCalculator sut = new PhoneBillCalculator();
 		Account account = new Account() {
 			@Override
 			public double getBasicMonthlyRate() {
-				return 0;
+				return 3;
 			}
 			@Override
 			public double getRateForAdditionalLines() {
-				return 0;
+				return 3;
 			}
 			@Override
 			public double getRateForExcessMinutes(int usedMinutes) {
-				return 0;
+				return 3;
 			}
 		};
-		assertEquals(0d, sut.getBill(account, 0), 0);
+		assertEquals(9d, sut.getBill(account, 0), 0);
 	}
 }

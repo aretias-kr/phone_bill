e37179892658aac3ecd961754a2db4db899c4b9c
JeongInn Lee <aretias.kr@gmail.com>
2013-10-14 01:59:46 +0900

    Refactoring Test to remove all duplicated code

--- src/test/java/PhoneBillCalculatorTest.java
+++ src/test/java/PhoneBillCalculatorTest.java
@@ -1,47 +1,46 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class PhoneBillCalculatorTest {
+	private PhoneBillCalculator sut;
+	private Account account;
+
+	private int basicMonthlyRate = 0;
+	private int rateForAdditionalLines = 0;
+	private int rateForExcessMinutes = 0;
+	
+	@Before
+	public void before() {
+		sut = new PhoneBillCalculator();
+		account = new FakeAccount();
+	}
+	private class FakeAccount extends Account {
+		@Override
+		public double getBasicMonthlyRate() {
+			return basicMonthlyRate;
+		}
+		@Override
+		public double getRateForAdditionalLines() {
+			return rateForAdditionalLines;
+		}
+		@Override
+		public double getRateForExcessMinutes(int usedMinutes) {
+			return rateForExcessMinutes;
+		}
+	}
 	@Test
 	public void should_return_0_when_every_rate_is_0() throws Exception {
-		PhoneBillCalculator sut = new PhoneBillCalculator();
-		Account account = new Account() {
-			@Override
-			public double getBasicMonthlyRate() {
-				return 0;
-			}
-			@Override
-			public double getRateForAdditionalLines() {
-				return 0;
-			}
-			@Override
-			public double getRateForExcessMinutes(int usedMinutes) {
-				return 0;
-			}
-			
-		};
 		assertEquals(0d, sut.getBill(account, 0), 0);
 	}
 	
 	@Test
 	public void should_return_9_when_every_rate_is_3() throws Exception {
-		PhoneBillCalculator sut = new PhoneBillCalculator();
-		Account account = new Account() {
-			@Override
-			public double getBasicMonthlyRate() {
-				return 3;
-			}
-			@Override
-			public double getRateForAdditionalLines() {
-				return 3;
-			}
-			@Override
-			public double getRateForExcessMinutes(int usedMinutes) {
-				return 3;
-			}
-		};
+		basicMonthlyRate = 3;
+		rateForAdditionalLines = 3;
+		rateForExcessMinutes = 3;
 		assertEquals(9d, sut.getBill(account, 0), 0);
 	}
 }

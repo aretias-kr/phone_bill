3c192f47e5357464427d43c7f6f2d0f2bb95e2aa
JeongInn Lee <aretias.kr@gmail.com>
2013-10-14 01:41:37 +0900

    add failing test to introduce fake object

--- src/test/java/PhoneBillCalculatorTest.java
+++ src/test/java/PhoneBillCalculatorTest.java
@@ -24,4 +24,24 @@ public class PhoneBillCalculatorTest {
 		};
 		assertEquals(0d, sut.getBill(account, 0), 0);
 	}
+	
+	@Test
+	public void should_return_0_when_basicRate_0_additionalLinesRate_0_and_excessMinutesRate_is_3() throws Exception {
+		PhoneBillCalculator sut = new PhoneBillCalculator();
+		Account account = new Account() {
+			@Override
+			public double getBasicMonthlyRate() {
+				return 0;
+			}
+			@Override
+			public double getRateForAdditionalLines() {
+				return 0;
+			}
+			@Override
+			public double getRateForExcessMinutes(int usedMinutes) {
+				return 0;
+			}
+		};
+		assertEquals(0d, sut.getBill(account, 0), 0);
+	}
 }
